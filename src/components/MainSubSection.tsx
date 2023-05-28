f5f483e6ca28f220831c819a514d4bab58e0ec7d
Suhwan0818 <tnghks1997@gmail.com>
2023-05-28 13:31:26 +0900

    ♻️ divide Main Page to components

---
+++ src/components/MainSubSection.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainSubSection: React.FC = () => {
+  return (
+    <div className="bg-main-blue rounded w-40 h-40">
+      <div className="flex h-full justify-center items-center text-main-white">
+        123
+      </div>
+    </div>
+  );
+};
+
+export default MainSubSection;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
-import {FaList} from 'react-icons/fa';
-import '../css/bounce-horizontal.css';
+import MainTitle from '../components/MainTitle';
+import MainSubSection from '../components/MainSubSection';
 
 const Main: React.FC = () => {
   const [isHovered, setIsHovered] = useState(false);
+  const [homeIsHovered, setHomeIsHovered] = useState(false);
+  const [contentsIsHovered, setContentsIsHovered] = useState(false);
 
   const handleMouseEnter = () => {
     setIsHovered(true);
@@ -12,48 +14,38 @@ const Main: React.FC = () => {
   const handleMouseLeave = () => {
     setIsHovered(false);
   };
+
+  const homeHandleMouseEnter = () => {
+    setHomeIsHovered(true);
+  };
+
+  const homeHandleMouseLeave = () => {
+    setHomeIsHovered(false);
+  };
+
+  const contentsHandleMouseEnter = () => {
+    setContentsIsHovered(true);
+  };
+
+  const contentsHandleMouseLeave = () => {
+    setContentsIsHovered(false);
+  };
+
   return (
-    <div className="flex min-h-screen m-0 bg-main-white">
-      <div
-        className={`w-1/2 bg-main-blue ${
-          isHovered ? 'animate-bounce-horizontal' : ''
-        }`}
-      >
-        <div className="flex justify-center items-center min-h-screen">
-          <div>
-            <div className="text-white text-8xl font-thin">꿈에</div>
-            <div className="text-main-green text-8xl font-thin">GREEN</div>
-            <div className="text-white text-8xl font-thin">삶</div>
-          </div>
-        </div>
-      </div>
-      <div className="relative w-1/2">
-        <div
-          className={`flex flex-row justify-between px-16 py-10 ${
-            isHovered ? 'animate-bounce-horizontal' : ''
-          }`}
-        >
-          <div className="flex flex-row justify-between w-40">
-            <div className="font-black text-main-blue">Home</div>
-            <div className="font-black text-main-blue">Contents</div>
-          </div>
-          <div className="flex justify-center items-center bg-main-blue rounded w-7 h-7 hover:animate-bounce-pulse m-0">
-            <FaList color="white" />
-          </div>
-        </div>
-        <div className="absolute top-1/2 translate-y-half-turn left-1/2 translate-x-half-turn">
-          <div className="flex flex-col items-center">
-            <div className="text-main-blue text-6xl">당신의 삶을</div>
-            <div className="text-main-blue text-6xl">의미있게</div>
-            <button
-              className="mt-10 px-5 py-2 bg-main-blue text-main-white font-black hover:animate-pulse"
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
-            >
-              JOIN US
-            </button>
-          </div>
-        </div>
+    <div className="flex flex-col min-h-screen m-0 bg-main-white">
+      <MainTitle
+        isHovered={isHovered}
+        homeIsHovered={homeIsHovered}
+        contentsIsHovered={contentsIsHovered}
+        handleMouseEnter={handleMouseEnter}
+        handleMouseLeave={handleMouseLeave}
+        homeHandleMouseEnter={homeHandleMouseEnter}
+        homeHandleMouseLeave={homeHandleMouseLeave}
+        contentsHandleMouseEnter={contentsHandleMouseEnter}
+        contentsHandleMouseLeave={contentsHandleMouseLeave}
+      />
+      <div className="flex">
+        <MainSubSection />
       </div>
     </div>
   );
