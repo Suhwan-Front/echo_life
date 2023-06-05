026f1532148403e3f0432531c4b26f697aa69f65
Suhwan0818 <tnghks1997@gmail.com>
2023-06-05 12:18:12 +0900

    🐛 dont care nothing

--- src/components/MainSubSection.tsx
+++ src/components/MainSubSection.tsx
@@ -8,6 +8,12 @@ const MainSubSection: React.FC = () => {
         <div className="flex justify-center bg-main-blue rounded w-40 h-40">
           <div className="flex h-full items-center text-main-white">123</div>
         </div>
+        <div className="flex justify-center bg-main-blue rounded w-40 h-40">
+          <div className="flex h-full items-center text-main-white">123</div>
+        </div>
+        <div className="flex justify-center bg-main-blue rounded w-40 h-40">
+          <div className="flex h-full items-center text-main-white">123</div>
+        </div>
       </div>
     </div>
   );
